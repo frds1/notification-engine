db916665f67147a85703dafc38d1ebc258444635
frds1 <feliperogerio.ti@gmail.com>
2026-02-13 14:39:30 -0300

    [NE-4] Implements db config

---
+++ internal/config/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"notification-engine/internal/config"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// NewGormConnection cria a conexão e configura o pool
+func NewGormConnection(cfg *config.Config) (*gorm.DB, error) {
+	databaseURL := "postgresql://" + cfg.DBUsername + ":" + cfg.DBPassword + "@" + cfg.DBHost + ":" + cfg.DBPort + "/"
+
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("falha ao conectar no gorm: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("falha ao obter instancia sql.DB: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, fmt.Errorf("banco de dados nao responde: %w", err)
+	}
+
+	log.Println("✅ Conexão GORM estabelecida e Pool configurado!")
+	return db, nil
+}
