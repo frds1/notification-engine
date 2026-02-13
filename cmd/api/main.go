1e650b31292a1eecde06865f52fe9423b2095f25
frds1 <feliperogerio.ti@gmail.com>
2026-02-13 14:15:43 -0300

    [NE-3] Implements load config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,16 @@
-package api
+package main
+
+import (
+	"log"
+	"notification-engine/internal/config"
+)
+
+func main() {
+	var err error
+
+	if err = config.Load(); err != nil {
+		log.Fatal("Erro ao carregar as envs")
+	}
+
+	log.Println("Hello World!")
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,62 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var cfg *Config
+
+type Config struct {
+	DBHost          string
+	DBUsername      string
+	DBPassword      string
+	DBPort          int64
+	NatsClientPort  int64
+	NatsMonitorPort int64
+}
+
+func Load() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Aviso: Arquivo .env não encontrado, usando variáveis de ambiente do sistema.")
+	}
+
+	cfg = &Config{
+		DBHost:          getEnv("DB_HOST", "localhost"),
+		DBUsername:      getEnv("DB_USER", "postgres"),
+		DBPassword:      getEnv("DB_PASSWORD", "postgres"),
+		DBPort:          getEnvAsInt("DB_PORT", 5432),
+		NatsClientPort:  getEnvAsInt("NATS_CLIENT_PORT", 4222),
+		NatsMonitorPort: getEnvAsInt("NATS_MONITOR_PORT", 8222),
+	}
+
+	return nil
+}
+
+func GetConfig() *Config {
+	return cfg
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
+
+func getEnvAsInt(key string, fallback int64) int64 {
+	valueStr, exists := os.LookupEnv(key)
+	if !exists {
+		return fallback
+	}
+	value, err := strconv.ParseInt(valueStr, 10, 64)
+	if err != nil {
+		log.Printf("Erro ao converter %s para int, usando default: %d", key, fallback)
+		return fallback
+	}
+	return value
+}
